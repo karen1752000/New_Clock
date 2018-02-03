f14e14a31c501c9213b73a0bb84ce29b42ffe72b
Karen Mitchell <karen1752000@yahoo.com>
2018-02-03 18:12:31 -0500

    Added min, sec, and hour hands

--- script.js
+++ script.js
@@ -13,19 +13,38 @@ function draw(){
     let sc = second();
 
     strokeWeight(8);
-    noFill();
     stroke(255, 100, 150);
-    let end1 = map(sc, 0, 60, 0, 360);
-    arc(0, 0, 300, 300, 0, end1);
-
+    noFill();
+    let secondAngle = map(sc, 0, 60, 0, 360);
+    arc(0, 0, 300, 300, 0, secondAngle);
+    
     stroke(150, 100, 255);
-    let end2 = map(mn, 0, 60, 0, 360);
-    arc(0, 0, 280, 280, 0, end2);
+    let minuteAngle = map(mn, 0, 60, 0, 360);
+    arc(0, 0, 280, 280, 0, minuteAngle);
 
     stroke(150, 255, 100);
-    let end3 = map(hr % 12, 0, 12, 0, 360);
-    arc(0, 0, 260, 260, 0, end3);
+    let hourAngle = map(hr % 12, 0, 12, 0, 360);
+    arc(0, 0, 260, 260, 0, hourAngle);
 
+    push();
+    rotate(secondAngle);
+    stroke(255, 100, 150);
+    line(0, 0, 100, 0);
+    pop();
+    
+    push();
+    rotate(minuteAngle);
+    stroke(150, 100, 255);
+    line(0, 0, 75, 0);
+    pop();
+
+    push();
+    rotate(hourAngle);
+    stroke(150, 255, 100);
+    line(0, 0, 50, 0);
+    pop();
+    
+    
     // strokeWeight(4);
     // stroke(255);
     // noFill();
