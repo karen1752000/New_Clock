3de9bc5d10744d1a4108f9a88a74f357f1999542
Karen Mitchell <karen1752000@yahoo.com>
2018-02-03 17:49:22 -0500

    rotated the clock to -90 degrees

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function setup() {
 
 function draw(){
     background(0);
+    translate(200, 200);
+    rotate(-90);
 
     let hr = hour();
     let mn = minute();
@@ -14,10 +16,15 @@ function draw(){
     noFill();
     stroke(255, 100, 150);
     let end1 = map(sc, 0, 60, 0, 360);
-    arc(200, 200, 300, 300, 0, end1);
+    arc(0, 0, 300, 300, 0, end1);
 
+    stroke(150, 100, 255);
     let end2 = map(mn, 0, 60, 0, 360);
-    arc(200, 200, 280, 280, 0, end2);
+    arc(0, 0, 280, 280, 0, end2);
+
+    stroke(150, 255, 100);
+    let end3 = map(hr % 12, 0, 12, -90, 360);
+    arc(0, 0, 260, 260, 0, end3);
 
     // strokeWeight(4);
     // stroke(255);
